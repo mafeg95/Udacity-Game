6bcecc64505dcc5417941a4e4028a36ca4a0c55b
Mafe Garcia <mfg21390@gmail.com>
2019-06-05 15:27:56 -0400

    player render function

--- js/app.js
+++ js/app.js
@@ -24,30 +24,20 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// // Enemies our player must avoid
-var Player = function(speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+
+var Player = function() {
     this.x = 3;
     this.y = 0;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/char-pink-girl.png';
 };
-//
-// // Update the enemy's position, required method for game
-// // Parameter: dt, a time delta between ticks
-// Player.prototype.update = function(dt) {
-//     // You should multiply any movement by the dt parameter
-//     // which will ensure the game runs at the same speed for
-//     // all computers.
-//     // this.y = dt * speed; // ?
-// };
-//
-// // Draw the enemy on the screen, required method for game
-// Player.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 //
 // // Now write your own player class
 // // This class requires an update(), render() and
