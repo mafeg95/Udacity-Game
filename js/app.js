7731955e56be573a75509e9e31df9782e1de0008
Mafe Garcia <mfg21390@gmail.com>
2019-06-07 19:17:25 -0400

    take out comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,18 @@
-// Enemies our player must avoid
+
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x = ((this.x + (this.speed * dt))); // ?
     if (this.x > 505){
       this.x = this.x - 1500;
     }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -55,14 +44,6 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 function getRandomInt(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -75,10 +56,9 @@ let enemy5 = new Enemy(getRandomInt(-1500, -2000), 138, getRandomInt(250, 400));
 let enemy6 = new Enemy(getRandomInt(-1500, -2000), 221, getRandomInt(250, 400));
 
 let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
-// let allEnemies = [enemy1];
+
 let player = new Player();
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
